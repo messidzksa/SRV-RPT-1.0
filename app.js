a47a1e04542961966cf719af1e70d1fe82919542
MohammedDarabie <darabiemohammad@gmail.com>
2025-10-10 13:17:35 +0300

    road to deployment

--- app.js
+++ app.js
@@ -39,9 +39,10 @@ app.all("*", (req, res, next) => {
 });
 app.use(globalErrorHandler);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
-
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`✅ Server running locally on http://localhost:${PORT}`);
+  });
+}
 module.exports = app;
