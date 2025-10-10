16e4b22f65ca818a48913d59ef526bfebe267801
MohammedDarabie <darabiemohammad@gmail.com>
2025-10-10 12:33:19 +0300

    change entry point server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
-// 🚀 Start the server
+
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
